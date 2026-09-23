3f585f6218c0a1898ec36c9b4d3dab3395eb201a
itskardy <kardelensumak@gmail.com>
2026-09-24 02:51:12 +0300

    Update status.js

--- functions/api/status.js
+++ functions/api/status.js
@@ -1,2 +1,17 @@
 import {response} from './_shared.js';
-export function onRequestGet({env}){return response({analytics:env.ANALYTICS_ENABLED==='true'&&!!env.HUN_ANALYTICS,feedback:!!(env.RESEND_API_KEY&&env.FEEDBACK_TO&&env.FEEDBACK_FROM&&env.FEEDBACK_RATE&&env.RATE_SALT)});}
+
+export function onRequestGet({env}) {
+  const checks = {
+    RESEND_API_KEY: !!env.RESEND_API_KEY,
+    RATE_SALT: !!env.RATE_SALT,
+    FEEDBACK_FROM: !!env.FEEDBACK_FROM,
+    FEEDBACK_TO: !!env.FEEDBACK_TO,
+    FEEDBACK_RATE: !!env.FEEDBACK_RATE
+  };
+
+  return response({
+    analytics: env.ANALYTICS_ENABLED === 'true' && !!env.HUN_ANALYTICS,
+    feedback: Object.values(checks).every(Boolean),
+    checks
+  });
+}
